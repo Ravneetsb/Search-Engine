d7928c0ff1d9ec64c1690f1ac7497dd30759c96f
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-02-09 14:45:29 -0800

    Implementing read functions

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -1,15 +1,22 @@
 package edu.usfca.cs272;
 
+import org.checkerframework.checker.units.qual.A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Class responsible for running this project based on the provided command-line
  * arguments. See the README for details.
  *
- * @author TODO Add Name Here
+ * @author Ravneet Singh Bhatia
  * @author CS 272 Software Development (University of San Francisco)
  * @version Spring 2024
  */
@@ -21,13 +28,22 @@ public class Driver {
 	 *
 	 * @param args flag/value pairs used to start this program
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// store initial start time
 		Instant start = Instant.now();
 
 		// TODO Fill in and modify as needed
+		ArgumentParser argParser = new ArgumentParser(args);
+		Path path = argParser.getPath("-text");
+		Path output = argParser.getPath("-count", Path.of("counts.json"));
 		System.out.println("Working Directory: " + Path.of(".").toAbsolutePath().normalize().getFileName());
 		System.out.println("Arguments: " + Arrays.toString(args));
+
+		if (Files.isDirectory(path)) {
+			readDirectory(path, output);
+		} else {
+			readFile(path, output);
+		}
 
 		// calculate time elapsed and output
 		long elapsed = Duration.between(start, Instant.now()).toMillis();
@@ -35,20 +51,29 @@ public class Driver {
 		System.out.printf("Elapsed: %f seconds%n", seconds);
 	}
 
-	/*
-	 * Generally, "Driver" classes are responsible for setting up and calling other
-	 * classes, usually from a main() method that parses command-line parameters.
-	 * Generalized reusable code are usually placed outside of the Driver class.
-	 * They are sometimes called "Main" classes too, since they usually include the
-	 * main() method.
-	 *
-	 * If the driver were only responsible for a single class, we use that class
-	 * name. For example, "TaxiDriver" is what we would name a driver class that
-	 * just sets up and calls the "Taxi" class.
-	 *
-	 * The starter code (calculating elapsed time) is not necessary. It can be
-	 * removed from the main method.
-	 *
-	 * TODO Delete this after reading.
-	 */
+	private static void readDirectory(Path path, Path output) throws IOException {
+
+		try (Stream<Path> files = Files.walk(path, Integer.MAX_VALUE, FileVisitOption.FOLLOW_LINKS)) {
+			files.forEach((filePath) -> {
+                try {
+                    readFile(filePath, output);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+		} catch (Exception e) {
+			throw new IOException();
+		}
+	}
+
+	private static void readFile(Path path, Path output) throws IOException {
+		Map<String, ? extends Number> map = new TreeMap<>();
+		try (BufferedReader br = Files.newBufferedReader(path)) {
+			String text;
+			while ((text = br.readLine()) != null) {
+				// Add stemming.
+			}
+		}
+	}
+
 }
