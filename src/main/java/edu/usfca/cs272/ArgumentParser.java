0bd9374a927f91416e5b0c9c477c3fe2ffafa174
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-02-22 19:34:54 -0800

    formatting code

--- src/main/java/edu/usfca/cs272/ArgumentParser.java
+++ src/main/java/edu/usfca/cs272/ArgumentParser.java
@@ -10,236 +10,232 @@ import java.util.HashMap;
  * @version Spring 2024
  */
 public class ArgumentParser {
-	/**
-	 * Stores command-line arguments in flag/value pairs.
-	 */
-	private final HashMap<String, String> map;
+  /** Stores command-line arguments in flag/value pairs. */
+  private final HashMap<String, String> map;
 
-	/**
-	 * Initializes this argument map.
-	 */
-	public ArgumentParser() {
-		this.map = new HashMap<>();
-	}
+  /** Initializes this argument map. */
+  public ArgumentParser() {
+    this.map = new HashMap<>();
+  }
 
-	/**
-	 * Initializes this argument map and then parsers the arguments into flag/value
-	 * pairs where possible. Some flags may not have associated values. If a flag is
-	 * repeated, its value is overwritten.
-	 *
-	 * @param args the command line arguments to parse
-	 */
-	public ArgumentParser(String[] args) {
-		this();
-		parse(args);
-	}
+  /**
+   * Initializes this argument map and then parsers the arguments into flag/value pairs where
+   * possible. Some flags may not have associated values. If a flag is repeated, its value is
+   * overwritten.
+   *
+   * @param args the command line arguments to parse
+   */
+  public ArgumentParser(String[] args) {
+    this();
+    parse(args);
+  }
 
-	/**
-	 * Determines whether the argument is a flag. The argument is considered a flag
-	 * if it is a dash "-" character followed by any character that is not a digit
-	 * or whitespace. For example, "-hello" and "-@world" are considered flags, but
-	 * "-10" and "- hello" are not.
-	 *
-	 * @param arg the argument to test if its a flag
-	 * @return {@code true} if the argument is a flag
-	 *
-	 * @see String#startsWith(String)
-	 * @see String#length()
-	 * @see String#codePointAt(int)
-	 * @see Character#isDigit(int)
-	 * @see Character#isWhitespace(int)
-	 */
-	public static boolean isFlag(String arg) {
-		if (arg != null && (arg.length() >= 2 && arg.startsWith("-"))) {
-			int second = arg.codePointAt(1);
-			return (!Character.isDigit(second) && !Character.isWhitespace(second));
-		}
-		return false;
-	}
+  /**
+   * Determines whether the argument is a flag. The argument is considered a flag if it is a dash
+   * "-" character followed by any character that is not a digit or whitespace. For example,
+   * "-hello" and "-@world" are considered flags, but "-10" and "- hello" are not.
+   *
+   * @param arg the argument to test if its a flag
+   * @return {@code true} if the argument is a flag
+   * @see String#startsWith(String)
+   * @see String#length()
+   * @see String#codePointAt(int)
+   * @see Character#isDigit(int)
+   * @see Character#isWhitespace(int)
+   */
+  public static boolean isFlag(String arg) {
+    if (arg != null && (arg.length() >= 2 && arg.startsWith("-"))) {
+      int second = arg.codePointAt(1);
+      return (!Character.isDigit(second) && !Character.isWhitespace(second));
+    }
+    return false;
+  }
 
-	/**
-	 * Determines whether the argument is a value. Anything that is not a flag is
-	 * considered a value.
-	 *
-	 * @param arg the argument to test if its a value
-	 * @return {@code true} if the argument is a value
-	 */
-	public static boolean isValue(String arg) {
-		return !isFlag(arg);
-	}
+  /**
+   * Determines whether the argument is a value. Anything that is not a flag is considered a value.
+   *
+   * @param arg the argument to test if its a value
+   * @return {@code true} if the argument is a value
+   */
+  public static boolean isValue(String arg) {
+    return !isFlag(arg);
+  }
 
-	/**
-	 * Parses the arguments into flag/value pairs where possible. Some flags may not
-	 * have associated values. If a flag is repeated, its value will be overwritten.
-	 *
-	 * @param args the command line arguments to parse
-	 */
-	public void parse(String[] args) {
-		if (args.length == 0)
-			return;
-		int i;
-		for (i = 1; i < args.length; i++) {
-			if (isFlag(args[i-1]) && isValue(args[i])) {
-				this.map.put(args[i-1], args[i]);
-			} else if (isFlag(args[i-1]))
-				this.map.put(args[i-1], null);
-		}
-		if (isFlag(args[i-1])) {
-			this.map.put(args[i-1], null);
-		}
-	}
+  /**
+   * Parses the arguments into flag/value pairs where possible. Some flags may not have associated
+   * values. If a flag is repeated, its value will be overwritten.
+   *
+   * @param args the command line arguments to parse
+   */
+  public void parse(String[] args) {
+    if (args.length == 0) return;
+    int i;
+    for (i = 1; i < args.length; i++) {
+      if (isFlag(args[i - 1]) && isValue(args[i])) {
+        this.map.put(args[i - 1], args[i]);
+      } else if (isFlag(args[i - 1])) this.map.put(args[i - 1], null);
+    }
+    if (isFlag(args[i - 1])) {
+      this.map.put(args[i - 1], null);
+    }
+  }
 
-	/**
-	 * Returns the number of unique flags.
-	 *
-	 * @return number of unique flags
-	 */
-	public int numFlags() {
-		return this.map.size();
-	}
+  /**
+   * Returns the number of unique flags.
+   *
+   * @return number of unique flags
+   */
+  public int numFlags() {
+    return this.map.size();
+  }
 
-	/**
-	 * Determines whether the specified flag exists.
-	 *
-	 * @param flag the flag check
-	 * @return {@code true} if the flag exists
-	 */
-	public boolean hasFlag(String flag) {
-		return this.map.containsKey(flag);
-	}
+  /**
+   * Determines whether the specified flag exists.
+   *
+   * @param flag the flag check
+   * @return {@code true} if the flag exists
+   */
+  public boolean hasFlag(String flag) {
+    return this.map.containsKey(flag);
+  }
 
-	/**
-	 * Determines whether the specified flag is mapped to a non-null value.
-	 *
-	 * @param flag the flag to find
-	 * @return {@code true} if the flag is mapped to a non-null value
-	 */
-	public boolean hasValue(String flag) {
-		return this.map.get(flag) != null;
-	}
+  /**
+   * Determines whether the specified flag is mapped to a non-null value.
+   *
+   * @param flag the flag to find
+   * @return {@code true} if the flag is mapped to a non-null value
+   */
+  public boolean hasValue(String flag) {
+    return this.map.get(flag) != null;
+  }
 
-	/**
-	 * Returns the value to which the specified flag is mapped as a {@link String}
-	 * or the backup value if there is no mapping.
-	 *
-	 * @param flag the flag whose associated value is to be returned
-	 * @param backup the backup value to return if there is no mapping
-	 * @return the value to which the specified flag is mapped, or the backup value
-	 *   if there is no mapping
-	 */
-	public String getString(String flag, String backup) {
-		String value = this.map.get(flag);
-		if (value != null) {
-			return value;
-		} else return backup;
-	}
+  /**
+   * Returns the value to which the specified flag is mapped as a {@link String} or the backup value
+   * if there is no mapping.
+   *
+   * @param flag the flag whose associated value is to be returned
+   * @param backup the backup value to return if there is no mapping
+   * @return the value to which the specified flag is mapped, or the backup value if there is no
+   *     mapping
+   */
+  public String getString(String flag, String backup) {
+    String value = this.map.get(flag);
+    if (value != null) {
+      return value;
+    } else return backup;
+  }
 
-	/**
-	 * Returns the value to which the specified flag is mapped as a {@link String}
-	 * or null if there is no mapping.
-	 *
-	 * @param flag the flag whose associated value is to be returned
-	 * @return the value to which the specified flag is mapped or {@code null} if
-	 *   there is no mapping
-	 */
-	public String getString(String flag) {
-		return this.map.getOrDefault(flag, null);
-	}
+  /**
+   * Returns the value to which the specified flag is mapped as a {@link String} or null if there is
+   * no mapping.
+   *
+   * @param flag the flag whose associated value is to be returned
+   * @return the value to which the specified flag is mapped or {@code null} if there is no mapping
+   */
+  public String getString(String flag) {
+    return this.map.getOrDefault(flag, null);
+  }
 
-	/**
-	 * Returns the value the specified flag is mapped as a {@link Path}, or the
-	 * backup value if unable to retrieve this mapping (including being unable to
-	 * convert the value to a {@link Path} or if no value exists).
-	 *
-	 * This method should not throw any exceptions!
-	 *
-	 * @param flag the flag whose associated value will be returned
-	 * @param backup the backup value to return if there is no valid mapping
-	 * @return the value the specified flag is mapped as a {@link Path}, or the
-	 *   backup value if there is no valid mapping
-	 *
-	 * @see Path#of(String, String...)
-	 */
-	public Path getPath(String flag, Path backup) {
-		try {
-            return Path.of(this.map.get(flag));
-		} catch (Exception e) {
-			return backup;
-		}
-	}
+  /**
+   * Returns the value the specified flag is mapped as a {@link Path}, or the backup value if unable
+   * to retrieve this mapping (including being unable to convert the value to a {@link Path} or if
+   * no value exists).
+   *
+   * <p>This method should not throw any exceptions!
+   *
+   * @param flag the flag whose associated value will be returned
+   * @param backup the backup value to return if there is no valid mapping
+   * @return the value the specified flag is mapped as a {@link Path}, or the backup value if there
+   *     is no valid mapping
+   * @see Path#of(String, String...)
+   */
+  public Path getPath(String flag, Path backup) {
+    try {
+      return Path.of(this.map.get(flag));
+    } catch (Exception e) {
+      return backup;
+    }
+  }
 
-	/**
-	 * Returns the value to which the specified flag is mapped as a {@link Path}, or
-	 * {@code null} if unable to retrieve this mapping (including being unable to
-	 * convert the value to a {@link Path} or no value exists).
-	 *
-	 * This method should not throw any exceptions!
-	 *
-	 * @param flag the flag whose associated value is to be returned
-	 * @return the value to which the specified flag is mapped, or {@code null} if
-	 *   unable to retrieve this mapping
-	 *
-	 * @see #getPath(String, Path)
-	 */
-	public Path getPath(String flag) {
-		return getPath(flag, null);
-	}
+  /**
+   * Returns the value to which the specified flag is mapped as a {@link Path}, or {@code null} if
+   * unable to retrieve this mapping (including being unable to convert the value to a {@link Path}
+   * or no value exists).
+   *
+   * <p>This method should not throw any exceptions!
+   *
+   * @param flag the flag whose associated value is to be returned
+   * @return the value to which the specified flag is mapped, or {@code null} if unable to retrieve
+   *     this mapping
+   * @see #getPath(String, Path)
+   */
+  public Path getPath(String flag) {
+    return getPath(flag, null);
+  }
 
-	/**
-	 * Returns the value the specified flag is mapped as an int value, or the backup
-	 * value if unable to retrieve this mapping (including being unable to convert
-	 * the value to an int or if no value exists).
-	 *
-	 * @param flag the flag whose associated value will be returned
-	 * @param backup the backup value to return if there is no valid mapping
-	 * @return the value the specified flag is mapped as an int, or the backup value
-	 *   if there is no valid mapping
-	 *
-	 * @see Integer#parseInt(String)
-	 */
-	public int getInteger(String flag, int backup) {
-		try {
-            return Integer.parseInt(this.map.get(flag));
-		} catch (Exception e) {
-			return backup;
-		}
-	}
+  /**
+   * Returns the value the specified flag is mapped as an int value, or the backup value if unable
+   * to retrieve this mapping (including being unable to convert the value to an int or if no value
+   * exists).
+   *
+   * @param flag the flag whose associated value will be returned
+   * @param backup the backup value to return if there is no valid mapping
+   * @return the value the specified flag is mapped as an int, or the backup value if there is no
+   *     valid mapping
+   * @see Integer#parseInt(String)
+   */
+  public int getInteger(String flag, int backup) {
+    try {
+      return Integer.parseInt(this.map.get(flag));
+    } catch (Exception e) {
+      return backup;
+    }
+  }
 
-	/**
-	 * Returns the value the specified flag is mapped as an int value, or 0 if
-	 * unable to retrieve this mapping (including being unable to convert the value
-	 * to an int or if no value exists).
-	 *
-	 * @param flag the flag whose associated value will be returned
-	 * @return the value the specified flag is mapped as an int, or 0 if there is no
-	 *   valid mapping
-	 *
-	 * @see #getInteger(String, int)
-	 */
-	public int getInteger(String flag) {
-		return getInteger(flag, 0);
-	}
+  /**
+   * Returns the value the specified flag is mapped as an int value, or 0 if unable to retrieve this
+   * mapping (including being unable to convert the value to an int or if no value exists).
+   *
+   * @param flag the flag whose associated value will be returned
+   * @return the value the specified flag is mapped as an int, or 0 if there is no valid mapping
+   * @see #getInteger(String, int)
+   */
+  public int getInteger(String flag) {
+    return getInteger(flag, 0);
+  }
 
-	@Override
-	public String toString() {
-		return this.map.toString();
-	}
+  @Override
+  public String toString() {
+    return this.map.toString();
+  }
 
-	/**
-	 * Demonstrates this class.
-	 *
-	 * @param args the arguments to test
-	 */
-	public static void main(String[] args) {
-		// Feel free to modify or delete this method for debugging
-		if (args.length < 1) {
-			args = new String[] { "-max", "false", "-min", "0", "-min", "-10", "hello", "-@debug",
-					"-f", "output.txt", "-verbose" };
-		}
+  /**
+   * Demonstrates this class.
+   *
+   * @param args the arguments to test
+   */
+  public static void main(String[] args) {
+    // Feel free to modify or delete this method for debugging
+    if (args.length < 1) {
+      args =
+          new String[] {
+            "-max",
+            "false",
+            "-min",
+            "0",
+            "-min",
+            "-10",
+            "hello",
+            "-@debug",
+            "-f",
+            "output.txt",
+            "-verbose"
+          };
+    }
 
-		// expected output:
-		// {-max=false, -min=-10, -verbose=null, -f=output.txt, -@debug=null}
-		ArgumentParser map = new ArgumentParser(args);
-		System.out.println(map);
-	}
+    // expected output:
+    // {-max=false, -min=-10, -verbose=null, -f=output.txt, -@debug=null}
+    ArgumentParser map = new ArgumentParser(args);
+    System.out.println(map);
+  }
 }
--- src/main/java/edu/usfca/cs272/WordCounter.java
+++ src/main/java/edu/usfca/cs272/WordCounter.java
@@ -7,22 +7,26 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class WordCounter {
-    private final Map<String, Integer> map;
+  private final Map<String, Integer> map;
 
-    public WordCounter() {
-        this.map = new TreeMap<>();
-    }
+  public WordCounter() {
+    this.map = new TreeMap<>();
+  }
 
-    public Map<String, Integer> getMap() {
-        return Collections.unmodifiableMap(this.map);
-    }
+  public Map<String, Integer> getMap() {
+    return Collections.unmodifiableMap(this.map);
+  }
 
-    public void write(Path output) throws IOException {
-        JsonWriter.writeObject(this.map, output);
-    }
+  public void write(Path output) throws IOException {
+    JsonWriter.writeObject(this.map, output);
+  }
 
-    public void compute(Path path, String data) {
-        this.map.compute(String.valueOf(path), (key, value) -> (value == null) ? FileStemmer.listStems(data).size() : FileStemmer.listStems(data).size() + value);
-    }
+  public void compute(Path path, String data) {
+    this.map.compute(
+        String.valueOf(path),
+        (key, value) ->
+            (value == null)
+                ? FileStemmer.listStems(data).size()
+                : FileStemmer.listStems(data).size() + value);
+  }
 }
-
