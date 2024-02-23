79f4c26add0c9c60a6186881feff7a38f4321a88
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-02-22 19:31:32 -0800

    Fixing the messed up merge

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -7,18 +7,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Arrays;
+import java.util.*;
 
 /**
- * Class responsible for running this project based on the provided command-line arguments. See the
- * README for details.
+ * Class responsible for running this project based on the provided command-line
+ * arguments. See the README for details.
  *
  * @author Ravneet Singh Bhatia
  * @author CS 272 Software Development (University of San Francisco)
  * @version Spring 2024
  */
 public class Driver {
-
 	/**
 	 * Initializes the classes necessary based on the provided command-line
 	 * arguments. This includes (but is not limited to) how to build or search an
@@ -26,27 +25,9 @@ public class Driver {
 	 *
 	 * @param args flag/value pairs used to start this program
 	 */
-	public static void main(String[] args) throws IOException { // TODO Remove throws IOException
+	public static void main(String[] args) throws IOException {
 		// store initial start time
 		Instant start = Instant.now();
-
-		/* TODO
-		ArgumentParser parser = new ArgumentParser(args);
-		InvertedIndex index = new InvertedIndex();
-
-		if (parser.hasFlag("-text")) {
-			Path path = parser.getPath("-text");
-
-			try {
-				1 line of code
-			}
-			catch ( ) {
-				System.out.println("Unable to build the index from path: " + path);
-			}
-		}
-
-		one per flag/value pair
-		 */
 
 		ArgumentParser argParser = new ArgumentParser(args);
 		try {
@@ -55,12 +36,10 @@ public class Driver {
 			Path countOutput = argParser.hasFlag("-counts") ? argParser.getPath("-counts", Path.of("counts.json")) : null;
 			indexOutput = argParser.hasFlag("-index") ? argParser.getPath("-index", Path.of("index.json")) : null;
 			if (path == null) {
-				if (countOutput != null) {
+				if (countOutput != null)
 					Files.createFile(Path.of("counts.json"));
-				}
-				if (indexOutput != null) {
+				if (indexOutput != null)
 					Files.createFile(Path.of("index.json"));
-				}
 			}
 			System.out.println("Using " + indexOutput);
 			System.out.println("Working Directory: " + Path.of(".").toAbsolutePath().normalize().getFileName());
@@ -68,7 +47,7 @@ public class Driver {
 			WordCounter counter = new WordCounter();
 			InvertedIndex index = new InvertedIndex();
 
-            if (Files.isDirectory(path)) { // TODO Mix of tabs and spaces
+            if (Files.isDirectory(path)) {
 				readDirectory(path, countOutput, counter, indexOutput, index);
 			} else {
 				readFile(path, counter, indexOutput,  index);
@@ -79,60 +58,13 @@ public class Driver {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-  
-  /**
-   * Initializes the classes necessary based on the provided command-line arguments. This includes
-   * (but is not limited to) how to build or search an inverted index.
-   *
-   * @param args flag/value pairs used to start this program
-   */
-  public static void main(String[] args) throws IOException {
-    // store initial start time
-    Instant start = Instant.now();
 
-    ArgumentParser argParser = new ArgumentParser(args);
-    try {
-      Path path = null;
-      Path indexOutput = null;
-      path = argParser.getPath("-text");
-      Path countOutput =
-          argParser.hasFlag("-counts")
-              ? argParser.getPath("-counts", Path.of("counts.json"))
-              : null;
-      indexOutput =
-          argParser.hasFlag("-index") ? argParser.getPath("-index", Path.of("index.json")) : null;
-      if (path == null) {
-        if (countOutput != null) Files.createFile(Path.of("counts.json"));
-        if (indexOutput != null) Files.createFile(Path.of("index.json"));
-      }
-      System.out.println("Using " + indexOutput);
-      System.out.println(
-          "Working Directory: " + Path.of(".").toAbsolutePath().normalize().getFileName());
-      System.out.println("Arguments: " + Arrays.toString(args));
-      WordCounter counter = new WordCounter();
-      InvertedIndex index = new InvertedIndex();
+		// calculate time elapsed and output
+		long elapsed = Duration.between(start, Instant.now()).toMillis();
+		double seconds = (double) elapsed / Duration.ofSeconds(1).toMillis();
+		System.out.printf("Elapsed: %f seconds%n", seconds);
+	}
 
-      if (Files.isDirectory(path)) {
-        readDirectory(path, countOutput, counter, indexOutput, index);
-      } else {
-        readFile(path, counter, indexOutput, index);
-        if (countOutput != null) {
-          JsonWriter.writeObject(counter.getMap(), countOutput);
-        }
-      }
-    } catch (Exception e) {
-      System.out.println(e);
-    }
-
-
-    // calculate time elapsed and output
-    long elapsed = Duration.between(start, Instant.now()).toMillis();
-    double seconds = (double) elapsed / Duration.ofSeconds(1).toMillis();
-    System.out.printf("Elapsed: %f seconds%n", seconds);
-  }
-
-
-	// TODO  MOve into new builder class
 	private static void readDirectory(Path input, Path output, WordCounter counter, Path indexOutput, InvertedIndex index) throws IOException {
 		try (DirectoryStream<Path> listing = Files.newDirectoryStream(input)) {
 			for (Path path: listing) {
@@ -141,16 +73,15 @@ public class Driver {
 				} else {
 					if (fileIsTXT(path)) {
 						readFile(path, counter, indexOutput, index);
-						if (output != null) {
+						if (output != null)
 							counter.write(output);
-						}
 					}
 				}
 			}
 		}
 	}
 
-	// TODO  MOve into new builder class
+
 	private static void readFile(Path path, WordCounter counter, Path indexOutput, InvertedIndex index) throws IOException {
         try (BufferedReader br = Files.newBufferedReader(path)) {
 			String text;
@@ -166,79 +97,11 @@ public class Driver {
 		}
 	}
 
-	private static boolean fileIsTXT(Path path) { // TODO Move into the builder
+	private static boolean fileIsTXT(Path path) {
 		return path.toString().toLowerCase().endsWith(".txt") || path.toString().toLowerCase().endsWith(".text");
 	}
 
 	// CITE: Talked to Frank about not having multi-line reading.
-=======
-  private static void readDirectory(
-      Path input, Path output, WordCounter counter, Path indexOutput, InvertedIndex index)
-      throws IOException {
-    try (DirectoryStream<Path> listing = Files.newDirectoryStream(input)) {
-      for (Path path : listing) {
-        if (Files.isDirectory(path)) {
-          readDirectory(path, output, counter, indexOutput, index);
-        } else {
-          if (fileIsTXT(path)) {
-            readFile(path, counter, indexOutput, index);
-            if (output != null) counter.write(output);
-          }
-        }
-      }
-    }
-  }
 
-  private static void readFile(
-      Path path, WordCounter counter, Path indexOutput, InvertedIndex index) throws IOException {
-    try (BufferedReader br = Files.newBufferedReader(path)) {
-      String text;
-      int iter = 0;
-      while ((text = br.readLine()) != null) {
-        if (!text.isEmpty()) {
-          counter.compute(path, text);
-          iter = index.index(path, text, iter);
-        }
-      }
-      index.write(indexOutput);
-    }
-  }
-
-  private static boolean fileIsTXT(Path path) {
-    return path.toString().toLowerCase().endsWith(".txt")
-        || path.toString().toLowerCase().endsWith(".text");
-  }
-
-  // CITE: Talked to Frank about not having multi-line reading.
-
+	//Test comment
 }
-
-/*
-TODO
-Description	Resource	Path	Location	Type
-Javadoc: Missing comment for private declaration	Driver.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 68	Java Problem
-Javadoc: Missing comment for private declaration	Driver.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 85	Java Problem
-Javadoc: Missing comment for private declaration	Driver.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 100	Java Problem
-Javadoc: Missing comment for private declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 8	Java Problem
-Javadoc: Missing comment for private declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 42	Java Problem
-Javadoc: Missing comment for private declaration	WordCounter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 10	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 7	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 10	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 14	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 18	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 22	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 33	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 49	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 53	Java Problem
-Javadoc: Missing comment for public declaration	InvertedIndex.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 57	Java Problem
-Javadoc: Missing comment for public declaration	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 364	Java Problem
-Javadoc: Missing comment for public declaration	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 374	Java Problem
-Javadoc: Missing comment for public declaration	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 383	Java Problem
-Javadoc: Missing comment for public declaration	WordCounter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 9	Java Problem
-Javadoc: Missing comment for public declaration	WordCounter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 12	Java Problem
-Javadoc: Missing comment for public declaration	WordCounter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 16	Java Problem
-Javadoc: Missing comment for public declaration	WordCounter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 20	Java Problem
-Javadoc: Missing comment for public declaration	WordCounter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 24	Java Problem
-Javadoc: Missing tag for declared exception IOException	Driver.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 28	Java Problem
-The import java.util.concurrent.atomic.AtomicInteger is never used	JsonWriter.java	/SearchEngine/src/main/java/edu/usfca/cs272	line 11	Java Problem
-*/
--- src/main/java/edu/usfca/cs272/InvertedIndex.java
+++ src/main/java/edu/usfca/cs272/InvertedIndex.java
@@ -2,20 +2,10 @@ package edu.usfca.cs272;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
-
-// TODO Format code!
+import java.util.*;
 
 public class InvertedIndex {
-
     private final Map<String, Map<String, Collection<Integer>>> map;
-    // TODO private final Map<String, Integer> map;
 
     public InvertedIndex() {
         this.map = new TreeMap<>();
@@ -29,11 +19,6 @@ public class InvertedIndex {
         return Collections.unmodifiableMap(this.map.get(word));
     }
 
-    /*
-     * TODO Move all of the stemming into a new class
-     * Inside of a builder class
-     */
-
     public int index(Path path, String text, int arrSize) {
         ArrayList<String> stems = FileStemmer.listStems(text);
         int nextVal = 0;
@@ -43,22 +28,18 @@ public class InvertedIndex {
             this.compute(String.valueOf(path), stem, i+1 + arrSize);
             nextVal = i+1+arrSize;
         }
-        return nextVal==0?arrSize:nextVal;
+        return (nextVal==0)?arrSize:nextVal;
     }
     public void index(Path path, String text) {
-    	// TODO public void index(Path path, InvertedIndex index) {
-        ArrayList<String> stems = FileStemmer.listStems(text); // TODO listStems(path)
+        ArrayList<String> stems = FileStemmer.listStems(text);
         for (int i = 0; i < stems.size(); i++) {
             String stem = stems.get(i);
-            	// TODO index.add(stem, path.toString(), i + 1)
-            this.map.putIfAbsent(stem, new TreeMap<>()); // TODO Move into compute
+            this.map.putIfAbsent(stem, new TreeMap<>());
             this.compute(String.valueOf(path), stem, i+1);
         }
-
-        // TODO index.addCount(path.toString(), stems.size());
     }
 
-    private void compute(String path, String stem, int location) { // TODO add method, make public
+    private void compute(String path, String stem, int location) {
         var stemMap = this.map.get(stem);
         stemMap.putIfAbsent(path, new TreeSet<>());
         var locationSet = stemMap.get(path);
@@ -76,60 +57,5 @@ public class InvertedIndex {
     public boolean isEmpty() {
         return this.map.isEmpty();
     }
-
-
-  private final Map<String, Map<String, Collection<Integer>>> map;
-
-  public InvertedIndex() {
-    this.map = new TreeMap<>();
-  }
-
-  public Set<String> getWords() {
-    return Collections.unmodifiableSet(this.map.keySet());
-  }
-
-  public Map<String, Collection<? extends Number>> getPositions(String word) {
-    return Collections.unmodifiableMap(this.map.get(word));
-  }
-
-  public int index(Path path, String text, int arrSize) {
-    ArrayList<String> stems = FileStemmer.listStems(text);
-    int nextVal = 0;
-    for (int i = 0; i < stems.size(); i++) {
-      String stem = stems.get(i);
-      this.map.putIfAbsent(stem, new TreeMap<>());
-      this.compute(String.valueOf(path), stem, i + 1 + arrSize);
-      nextVal = i + 1 + arrSize;
-    }
-    return (nextVal == 0) ? arrSize : nextVal;
-  }
-
-  public void index(Path path, String text) {
-    ArrayList<String> stems = FileStemmer.listStems(text);
-    for (int i = 0; i < stems.size(); i++) {
-      String stem = stems.get(i);
-      this.map.putIfAbsent(stem, new TreeMap<>());
-      this.compute(String.valueOf(path), stem, i + 1);
-    }
-  }
-
-  private void compute(String path, String stem, int location) {
-    var stemMap = this.map.get(stem);
-    stemMap.putIfAbsent(path, new TreeSet<>());
-    var locationSet = stemMap.get(path);
-    locationSet.add(location);
-  }
-
-  public void printMap() {
-    System.out.println(this.map);
-  }
-
-  public void write(Path path) throws IOException {
-    JsonWriter.writeIndex(this.map, path);
-  }
-
-  public boolean isEmpty() {
-    return this.map.isEmpty();
-  }
 
 }
