f7891a9331193cc24de2b59b5b12c79ca2e455e3
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-04-16 19:01:56 -0700

    Adding override methods to ThreadSafeInvertedIndex

--- src/main/java/edu/usfca/cs272/ThreadSafeInvertedIndex.java
+++ src/main/java/edu/usfca/cs272/ThreadSafeInvertedIndex.java
@@ -1,5 +1,10 @@
 package edu.usfca.cs272;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /** Thread Safe Inverted Index. */
@@ -69,6 +74,71 @@ public class ThreadSafeInvertedIndex extends InvertedIndex {
     } finally {
       lock.readLock().unlock();
     }
+  }
+
+  @Override
+  public Map<String, Integer> getCounts() {
+    return super.getCounts();
+  }
+
+  @Override
+  public boolean add(String stem, String path, int location) {
+    return super.add(stem, path, location);
+  }
+
+  @Override
+  public boolean addAll(String path, List<String> stems) {
+    return super.addAll(path, stems);
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return super.isEmpty();
+  }
+
+  @Override
+  public int size() {
+    return super.size();
+  }
+
+  @Override
+  public int sizeOfCounts() {
+    return super.sizeOfCounts();
+  }
+
+  @Override
+  public int numOfLocations(String word) {
+    return super.numOfLocations(word);
+  }
+
+  @Override
+  public int numOfPositions(String word, String location) {
+    return super.numOfPositions(word, location);
+  }
+
+  @Override
+  public boolean hasWord(String word) {
+    return super.hasWord(word);
+  }
+
+  @Override
+  public ArrayList<Score> search(Set<String> queries, boolean partial) {
+    return super.search(queries, partial);
+  }
+
+  @Override
+  public ArrayList<Score> exactSearch(Set<String> queries) {
+    return super.exactSearch(queries);
+  }
+
+  @Override
+  public ArrayList<Score> partialSearch(Set<String> queries) {
+    return super.partialSearch(queries);
+  }
+
+  @Override
+  public void toJson(Path output) throws IOException {
+    super.toJson(output);
   }
 
   @Override
