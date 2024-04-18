a571c9501afb5046d7ffa3484b640b1ff682b06b
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-04-18 13:27:22 -0700

    adding override methods

--- src/main/java/edu/usfca/cs272/ThreadSafeQueryProcessor.java
+++ src/main/java/edu/usfca/cs272/ThreadSafeQueryProcessor.java
@@ -1,7 +1,11 @@
 package edu.usfca.cs272;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.function.Function;
 
 /** Query Processor for multi-threaded search */
@@ -16,6 +20,9 @@ public class ThreadSafeQueryProcessor extends QueryProcessor {
   /** search method to be used. */
   private final Function<Set<String>, ArrayList<InvertedIndex.Score>> searchMethod;
 
+  /** The results of the search. */
+  private final TreeMap<String, ArrayList<InvertedIndex.Score>> searches;
+
   /**
    * Creates a new QueryProcessor
    *
@@ -28,6 +35,7 @@ public class ThreadSafeQueryProcessor extends QueryProcessor {
     this.index = invertedIndex;
     this.queue = new WorkQueue(threads);
     this.searchMethod = partial ? invertedIndex::partialSearch : invertedIndex::exactSearch;
+    this.searches = new TreeMap<>();
   }
 
   /**
@@ -41,5 +49,51 @@ public class ThreadSafeQueryProcessor extends QueryProcessor {
     this.index = invertedIndex;
     this.queue = new WorkQueue(threads);
     this.searchMethod = invertedIndex::partialSearch;
+    this.searches = new TreeMap<>();
+  }
+
+  @Override
+  public void parseQuery(Path query) throws IOException {
+    super.parseQuery(query);
+  }
+
+  @Override
+  public void parseQuery(String query) {
+    super.parseQuery(query);
+  }
+
+  @Override
+  public void toJson(Path path) throws IOException {
+    super.toJson(path);
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+
+  @Override
+  public int numOfResults() {
+    return super.numOfResults();
+  }
+
+  @Override
+  public int numOfScores(String query) {
+    return super.numOfScores(query);
+  }
+
+  @Override
+  public List<InvertedIndex.Score> getScores(String query) {
+    return super.getScores(query);
+  }
+
+  @Override
+  public Set<String> getQueries() {
+    return super.getQueries();
+  }
+
+  @Override
+  public boolean hasQuery(String query) {
+    return super.hasQuery(query);
   }
 }
