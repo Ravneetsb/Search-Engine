1c5694e2af36b7c11f497217422312e79b750044
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-04-19 10:55:44 -0700

    changing while to if and moving the decrementPending call

--- src/main/java/edu/usfca/cs272/WorkQueue.java
+++ src/main/java/edu/usfca/cs272/WorkQueue.java
@@ -139,7 +139,7 @@ public class WorkQueue {
       try {
         while (true) {
           synchronized (tasks) {
-            if (tasks.isEmpty() && !shutdown) {
+            while (tasks.isEmpty() && !shutdown) {
               tasks.wait();
             }
 
@@ -148,13 +148,13 @@ public class WorkQueue {
             }
 
             task = tasks.removeFirst();
-            decrementPending();
           }
           try {
             task.run();
           } catch (RuntimeException e) {
             log.catching(Level.ERROR, e);
           }
+          decrementPending();
         }
       } catch (InterruptedException e) {
         log.catching(Level.WARN, e);
