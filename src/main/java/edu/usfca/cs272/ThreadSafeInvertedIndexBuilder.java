065a9fd28bae5b2db187bdd75e0b07f2ab9bedf6
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-04-22 12:38:03 -0700

    calling finish instead of join for workqueue

--- src/main/java/edu/usfca/cs272/ThreadSafeInvertedIndexBuilder.java
+++ src/main/java/edu/usfca/cs272/ThreadSafeInvertedIndexBuilder.java
@@ -37,11 +37,12 @@ public class ThreadSafeInvertedIndexBuilder extends InvertedIndexBuilder {
     } else {
       queue.execute(new Task(input, index));
     }
-    queue.join(); // TODO finish, call join or shutdown in Driver
+    queue.finish();
   }
 
   @Override
-  public void readDirectory(Path directory) throws IOException { // TODO Remove this, inherit original
+  public void readDirectory(Path directory)
+      throws IOException { // TODO Remove this, inherit original
     try (DirectoryStream<Path> listing = Files.newDirectoryStream(directory)) {
       for (Path path : listing) {
         if (Files.isDirectory(path)) {
@@ -87,15 +88,16 @@ public class ThreadSafeInvertedIndexBuilder extends InvertedIndexBuilder {
 
     @Override
     public void run() {
-    	/* TODO This is the "easy" way to get the tests passing... but not THAT fast
-    	var stems = FileStemmer.listStems(path);
-    	index.addAll(path.toString(), stems);
-    	
-    	(Don't change anything, just an FYI.)
-    	*/
-    	
+      /* TODO This is the "easy" way to get the tests passing... but not THAT fast
+      var stems = FileStemmer.listStems(path);
+      index.addAll(path.toString(), stems);
+
+      (Don't change anything, just an FYI.)
+      */
+
       InvertedIndex localIndex = new InvertedIndex();
-      InvertedIndexBuilder localBuilder = new InvertedIndexBuilder(localIndex); // TODO Use the static method
+      InvertedIndexBuilder localBuilder =
+          new InvertedIndexBuilder(localIndex); // TODO Use the static method
       try {
         localBuilder.readFile(path);
       } catch (IOException e) {
