d31bbd533b3d5f21387b51cfd21e86a923fd22dc
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-05-15 18:32:37 -0700

    refactoring.
    Took 5 minutes

--- src/main/java/edu/usfca/cs272/SearchServer.java
+++ src/main/java/edu/usfca/cs272/SearchServer.java
@@ -16,22 +16,28 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-/** Server for the search engine. */
+/**
+ * Server for the Search Engine.
+ *
+ * @author Ravneet Singh Bhatia
+ * @version Spring 2024
+ */
 public class SearchServer {
 
   /** The server. */
   private final Server server;
 
-  /** The processor to use. */
-  private final Processor processor;
-
+  /** The work queue that executes the search tasks. */
   private final WorkQueue queue;
 
+  /** The theme for the web pages. The default theme is light. */
   public static String theme = "<html lang=\"en\" data-theme=\"light\">";
 
-  public static final String LIGHT_THEME = "<html lang=\"en\" data-theme=\"light\">";
+  /** The html line for the light theme. */
+  private static final String LIGHT_THEME = "<html lang=\"en\" data-theme=\"light\">";
 
-  public static final String DARK_THEME = "<html lang=\"en\" data-theme=\"dark\">";
+  /** The html line for the dark theme. */
+  private static final String DARK_THEME = "<html lang=\"en\" data-theme=\"dark\">";
 
   /** Base path for the resource templates. */
   public static final Path base = Path.of("src", "main", "resources", "template");
@@ -46,11 +52,11 @@ public class SearchServer {
   public SearchServer(int port, ThreadSafeInvertedIndex index, Processor processor, WorkQueue queue)
       throws IOException {
     DatabaseConnector db = new DatabaseConnector(Path.of("src/main/resources/database.properties"));
-    this.server = new Server(port);
-    this.processor = processor;
+    this.server = new Server(port);     // instantiate the Server.
     this.queue = queue;
     ServletHandler handler = new ServletHandler();
-    handler.addServletWithMapping(new ServletHolder(new SearchServlet(this.processor, db)), "/");
+    // Map the servlets.
+    handler.addServletWithMapping(new ServletHolder(new SearchServlet(processor, db)), "/");
     handler.addServletWithMapping(new ServletHolder(new SettingsServlet(db)), "/settings");
     handler.addServletWithMapping(new ServletHolder(new ShutdownServlet()), "/shutdown");
     handler.addServletWithMapping(new ServletHolder(new ThemeServlet(db)), "/theme-change");
@@ -58,6 +64,9 @@ public class SearchServer {
     server.setHandler(handler);
   }
 
+/**
+* Changes the theme of the webpages.
+*/
   public static void changeTheme() {
     if (theme.equals(LIGHT_THEME)) {
       theme = DARK_THEME;
@@ -81,51 +90,47 @@ public class SearchServer {
   }
 
   /**
-   * Shutdown servlet for the server. This allows the user to trigger a graceful shutdown by entering
-   * a password.
+   * Shutdown servlet for the server. This allows the user to trigger a graceful shutdown by
+   * entering a password.
    */
   public class ShutdownServlet extends HttpServlet {
     /** Class version for serialization, in [YEAR][TERM] format (unused). */
-    @Serial
-    private static final long serialVersionUID = 202401;
+    @Serial private static final long serialVersionUID = 202401;
 
     /** The html template to serve. */
     private final String htmlTemplate;
 
     /** The password to trigger the shutdown. */
-    private static final String PASSWORD = "a";
+    private static final String PASSWORD = "p@$$w0rd";
 
     /**
-     * The servlet for the shutdown page.
+     * Creates a new Shutdown servlet.
      *
      * @throws IOException if the template cannot be read.
      */
     public ShutdownServlet() throws IOException {
       this.htmlTemplate =
-              Files.readString(SearchServer.base.resolve("shutdown.html"), StandardCharsets.UTF_8);
+          Files.readString(SearchServer.base.resolve("shutdown.html"), StandardCharsets.UTF_8);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+        throws ServletException, IOException {
 
       String password = request.getParameter("password");
-      //    StringJoiner joiner = new StringJoiner("\n");
 
       response.setContentType("text/html");
       response.setStatus(HttpServletResponse.SC_OK);
-
 
       // output generated html
       PrintWriter out = response.getWriter();
       out.printf(htmlTemplate, SearchServer.theme);
       out.flush();
 
-      if (password != null) {
-        System.out.println(password);
-        if (password.equals(PASSWORD)) {
-          System.out.println(server);
+      if (password != null) {   // there is an input.
+        if (password.equals(PASSWORD)) {      // authenticate.
           try {
+            // Graceful shutdown 🧘‍♂️
             server.stop();
             server.join();
             queue.join();
@@ -133,7 +138,7 @@ public class SearchServer {
             System.err.println("Unable to shutdown server");
           }
         } else {
-
+            // Add pop-up if there is time.
         }
       }
     }
